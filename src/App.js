b6380b75bae09bf0511c2d4cd94a10b7b890038a
Kostiantyn <slimshady00713@gmail.com>
2023-08-23 16:20:43 +0300

    sidebar with popup completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import List from './components/List/List';
+import listSvg from './assests/img/list.svg';
+import AddList from './components/AddList/AddList';
+
+import DB from './assests/db.json';
+
+import './index.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__sidebar">
+        <List
+          items={[
+            {
+              icon: <img src={listSvg} alt="List icon" />,
+              name: 'All tasks',
+              active: false,
+            },
+          ]}
+        />
+        <List
+          items={[
+            {
+              color: 'green',
+              name: 'Покупки',
+            },
+            {
+              color: 'blue',
+              name: 'Фронтенд',
+              active: true,
+            },
+            {
+              color: 'pink',
+              name: 'Фильмы и сериалы',
+            },
+          ]}
+          isRemovable
+        />
+        <AddList colors={DB.colors} />
+      </div>
+      <div className="todo__tasks"></div>
     </div>
   );
 }
---
+++ src/components/AddList/AddList.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import List from '../List/List';
+import Badge from '../Badge/Badge';
+import CloseSvg from '../../assests/img/close.svg';
+
+import './AddList.scss';
+
+const AddList = ({ colors }) => {
+  const [visiblePopup, setVisiblePopup] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(colors[0].id);
+
+  return (
+    <div className="add-list">
+      <List
+        onClick={() => setVisiblePopup(true)}
+        items={[
+          {
+            className: 'list__add-button',
+            icon: (
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M6 1V11"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1 6H11"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ),
+            name: 'Add list',
+            active: false,
+          },
+        ]}
+      />
+      {visiblePopup && (
+        <div className="add-list__popup">
+          <img
+            onClick={() => setVisiblePopup(false)}
+            src={CloseSvg}
+            alt="Close button"
+            className="add-list__popup-close-btn"
+          />
+          <input className="field" type="text" placeholder="List name" />
+          <div className="add-list__popup-colors">
+            {colors.map((color) => (
+              <Badge
+                onClick={() => setSelectedColor(color.id)}
+                key={color.id}
+                color={color.name}
+                className={selectedColor === color.id && 'active'}
+              />
+            ))}
+          </div>
+          <button className="button">Add</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddList;
